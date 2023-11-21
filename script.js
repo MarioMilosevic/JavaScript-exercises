033f60f630e91e0624f9a5f96662e7eef47808e8
Mario Milošević <mariomilosevic887@gmail.com>
2023-11-21 22:00:00 +0100

    Finish exercise for spread and rest operators

--- script.js
+++ script.js
@@ -96,3 +96,86 @@
 // const tip = bill >= 50 && bill <=300 ? bill*0.15 : bill*0.2
 // console.log(`The bill was ${bill}, the tip was ${tip}, and the final value was ${bill + tip}`);
 // const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+
+// CHALLENGE 1 ///////////////////////////////////////////////////////////////////////
+
+// // 1. Create one player array for each team (variables 'players1' and 'players2')
+
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
+// // 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
+
+// // 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// //  4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+// const substitutes = ['Thiago', 'Coutinho', 'Perisic'];
+// const player1Final = [...players1, ...substitutes];
+// console.log(player1Final);
+
+// // 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// const { team1, x: draw, team2 } = game.odds;
+// console.log(team1, draw, team2);
+
+// // 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+
+// function printGoals(...players) {
+//   const goalsScored = players.length;
+//   const playersList = players.join(', ');
+//   console.log(`${goalsScored} goals were scored: ${playersList}`);
+// }
+
+// // Example usage:
+// // printGoals('Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels');
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich')
+// printGoals(...game.scored)
+
+// // 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator
+
+//  team1 < team2 && console.log(`Team 1 is more likely to win`)
+//  team1 > team2 && console.log(`Team 1 is more likely to win`)
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+      [
+        'Neuer',
+        'Pavard',
+        'Martinez',
+        'Alaba',
+        'Davies',
+        'Kimmich',
+        'Goretzka',
+        'Coman',
+        'Muller',
+        'Gnarby',
+        'Lewandowski',
+      ],
+      [
+        'Burki',
+        'Schulz',
+        'Hummels',
+        'Akanji',
+        'Hakimi',
+        'Weigl',
+        'Witsel',
+        'Hazard',
+        'Brandt',
+        'Sancho',
+        'Gotze',
+      ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+      team1: 1.33,
+      x: 3.25,
+      team2: 6.5,
+    },
+  };
