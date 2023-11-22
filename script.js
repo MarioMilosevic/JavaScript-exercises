f6f5b9433747a0c09cfbfc79cb8282a46e4402f2
Mario Milošević <mariomilosevic887@gmail.com>
2023-11-22 17:21:00 +0100

    Finish fourth exercise from second section

--- script.js
+++ script.js
@@ -370,3 +370,31 @@ GOOD LUCK 😀
 // }
 // // mark.calcBMI()
 // console.log(mark);
+
+//challenge 4 ////////////////////////////////////////////////////////////////////////
+
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+
+// for (let i = 0; i < bills.length; i++) {
+//   tips.push(calcTip(bills[i]));
+//   totals.push(bills[i] + tips[i]);
+// }
+// console.log(bills, tips);
+// console.log(totals);
+
+// const calcAverage = function (arr) {
+//   let sum = 0;
+//   let average = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   average = sum / arr.length;
+//   return average;
+// };
+// console.log(calcAverage(totals));
