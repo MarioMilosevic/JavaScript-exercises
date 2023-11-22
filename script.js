bc0d13f51d8b97e6811a0234ca3932ff6b91e6bf
Mario Milošević <mariomilosevic887@gmail.com>
2023-11-22 17:21:56 +0100

    Finish First exercise from 5th section

--- script.js
+++ script.js
@@ -398,3 +398,25 @@ GOOD LUCK 😀
 //   return average;
 // };
 // console.log(calcAverage(totals));
+
+// challenge #1 sekcija 5
+
+/* Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures. 
+Example: [17, 21, 23] will print 
+"... 17C in 1 days ...21C in 2 days ... 23C in 3 days ..."
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+Use the problem solving framweork: Understand the problem and break it up into sub-problems! 
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+// const printForecast = function(arr){
+//   let text = ''
+//   for (let i = 0; i < arr.length; i++) {
+//     text += `... ${arr[i]}C in ${i + 1} days `
+//   }
+//   return text + '...'
+// }
+
+// console.log(printForecast([17,21,23]));
+// console.log(printForecast([12,5,-5,0,4]));
