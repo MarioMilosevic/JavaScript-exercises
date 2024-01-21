6f9864b2a3d8f364759cfd4c9e2d15fbc2c3ad10
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-01-21 16:46:00 +0100

    Finish fetch exercise

--- script.js
+++ script.js
@@ -713,13 +713,18 @@ const renderCountry = (data, className = "") => {
 // console.log(request);
 
 const getCountryData = (country) => {
-  fetch(`https://restcountries.com/v2/name/${country}`).then((response) => {
-    console.log(response);
-    return response.json();
-  }).then((data) => {
-    console.log(data);
-    renderCountry(data[0])
-  })
+  // Country 1
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+      // Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`)
+        .then((response) => response.json())
+        .then((data) => renderCountry(data, "neighbour"));
+    });
 };
 
 getCountryData("montenegro");
