7d8e69bb492cdfd23b38eebd80b5758460ff7160
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-01-21 12:48:40 +0100

    Finish fetch lecture

--- script.js
+++ script.js
@@ -637,7 +637,7 @@ const countriesContainer = document.querySelector(".countries");
 // getCountryData('montenegro')
 // getCountryData('serbia')
 
-const renderCountry = (data, className = '') => {
+const renderCountry = (data, className = "") => {
   const html = `<article class="country ${className}">
     <img class="country__img" src="${data.flag}" />
     <div class="country__data">
@@ -654,52 +654,72 @@ const renderCountry = (data, className = '') => {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryDataAndNeighbour = (country) => {
-  // AJAX call country 1
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://restcountries.com/v2/name/${country}`);
-  request.send();
+// const getCountryDataAndNeighbour = (country) => {
+//   // AJAX call country 1
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.com/v2/name/${country}`);
+//   request.send();
 
-  request.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText);
+//   request.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     // Render country 1
+//     renderCountry(data);
+
+//     // Get neighbour country
+//     // const neighbour = data.borders?.[0]
+
+//     const [neighbour] = data.borders
+//     if(!neighbour) return
+
+//   // AJAX call country 2
+//     const request2 = new XMLHttpRequest();
+//   request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+//   request2.send();
+
+//   request2.addEventListener('load', function(){
+//     const data2 = JSON.parse(this.responseText)
+//     console.log(data2)
+
+//     renderCountry(data2, 'neighbour')
+//   })
+//   });
+// };
+
+// getCountryDataAndNeighbour('montenegro')
+
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 second passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//         setTimeout(() => {
+//           console.log('5 second passed');
+//         }, 1000)
+//       }, 1000)
+//     }, 1000)
+//   }, 1000)
+// }, 1000)
+
+// const request = new XMLHttpRequest();
+// request.open("GET", `https://restcountries.com/v2/name/${country}`);
+// request.send();
+
+// const request = fetch(`https://restcountries.com/v2/name/montenegro`);
+
+// console.log(request);
+
+const getCountryData = (country) => {
+  fetch(`https://restcountries.com/v2/name/${country}`).then((response) => {
+    console.log(response);
+    return response.json();
+  }).then((data) => {
     console.log(data);
-    // Render country 1
-    renderCountry(data);
-
-    // Get neighbour country
-    // const neighbour = data.borders?.[0]
-
-    const [neighbour] = data.borders
-    if(!neighbour) return
-
-  // AJAX call country 2
-    const request2 = new XMLHttpRequest();
-  request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
-  request2.send();
-
-  request2.addEventListener('load', function(){
-    const data2 = JSON.parse(this.responseText)
-    console.log(data2)
-
-    renderCountry(data2, 'neighbour')
+    renderCountry(data[0])
   })
-  });
 };
 
-getCountryDataAndNeighbour('montenegro')
-
-setTimeout(() => {
-  console.log('1 second passed');
-  setTimeout(() => {
-    console.log('2 second passed');
-    setTimeout(() => {
-      console.log('3 second passed');
-      setTimeout(() => {
-        console.log('4 second passed');
-        setTimeout(() => {
-          console.log('5 second passed');
-        }, 1000)
-      }, 1000)
-    }, 1000)
-  }, 1000)
-}, 1000)
+getCountryData("montenegro");
