a09ca7cb00315b96bec7c23064dd2df12fd9e1e3
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-09 10:25:54 +0100

    Add routes

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,11 @@
-console.log("js is loaded");
+'use strict'
+
+const router = async () => {
+    const routes = [
+        {path: '/', view: () => console.log('viewing dashboard')},
+        {path: '/posts', view: () => console.log('viewing posts')},
+        {path: '/settings', view: () => console.log('viewing settings')},
+    ]
+
+    // Test each root for potential match
+}
