80b1cee7c6f33fc59834119ee09e5a99ceac80b2
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-02-27 19:07:59 +0100

    Draw circle on canvas when clicked

--- canvas/script.js
+++ canvas/script.js
@@ -2,12 +2,31 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-console.log(ctx);
 
 window.addEventListener("resize", function () {
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
-
-  ctx.fillStyle = "white";
-  ctx.fillRect(10, 10, 150, 50);
 });
+
+const mouse = {
+  x: null,
+  y: null,
+};
+
+canvas.addEventListener("click", function (e) {
+  mouse.x = e.x;
+  mouse.y = e.y;
+  drawCircle(mouse);
+});
+
+function drawCircle(mouse) {
+   const {x,y} = mouse
+    ctx.fillStyle = "blue";
+  ctx.beginPath();
+  ctx.arc(x, y, 50, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+// ctx.strokeStyle = 'blue';
+// ctx.lineWidth = 5
+// ctx.stroke()
