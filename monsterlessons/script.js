cc6d4ecd6491d064ab8f54512d5e9e5fd87ce143
Mario Milošević <mario.milosevic@rbt.rs>
2025-11-01 19:35:27 +0100

    range function

--- monsterlessons/script.js
+++ monsterlessons/script.js
@@ -1,14 +1,13 @@
-//  Q2: Sort an array of objects by author’s lastname
-const books = [
-  { name: "Harry Potter", author: "Joanne Rowling" },
-  { name: "Warcross", author: "Marie Lu" },
-  { name: "THe Hunger Games", author: "Suzanne Collins" },
-];
+// Q1: Write a function which implements a range
+// console.log(range(1, 50)); // [1,2,3,4,5...,50]
 
-books.sort((a, b) => {
-  const firstLastName = a.author.split(" ")[1];
-  const secondLastName = b.author.split(" ")[1];
-  return firstLastName.localeCompare(secondLastName);
-});
+function range(from, to) {
+  const arr = [];
+  arr.push(from);
+  for (let i = from + 1; i <= to; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
 
-console.log(books);
+console.log(range(1, 50));
