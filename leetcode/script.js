348b3a114c4f04b8386d35e2cbd4b6a0f664331b
Mario Milošević <mario.milosevic@rbt.rs>
2025-11-01 18:32:49 +0100

    exercise

--- leetcode/script.js
+++
@@ -1,7 +0,0 @@
-function fibonacci(n) {
-  const fib = [0, 1];
-  for (let i = 2; i < n.length; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
-  }
-  return fib;
-}
---
+++ monsterlessons/script.js
@@ -0,0 +1,3 @@
+var foo;
+foo = 3;
+console.log(foo);
