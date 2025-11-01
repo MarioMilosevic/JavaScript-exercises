864e4063da7c2a6c8bd92f22df367f13a1e803c2
Mario Milošević <mario.milosevic@rbt.rs>
2025-11-01 21:37:37 +0100

    number of occurences

--- monsterlessons/script.js
+++ monsterlessons/script.js
@@ -1,13 +1,25 @@
-// ting Shuffle Function
-// – Q1: Write a shuffle function which mixes the elements
-// console.log(shuffleItems([1, 2]));
+// • Find the Number of Occurences of Minimum Value in List
+//  – Q1: Find the number of occurrences of minimum value in the list of numbers
 
-function shuffleItems(items) {
-  for (let i = items.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [items[i], items[j]] = [items[j], items[i]];
+const numbers = [3, 1, 2, 1, 4, 1, 1, 1, 1];
+
+// Step 1 → Find the minimum number in the array.
+const minNum = Math.min(...numbers);
+
+// Step 2 → Count how many times that number appears.
+
+// Step 3 → Return or print that count.
+
+function occurences(numbers) {
+  let count = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    const minNum = Math.min(...numbers);
+
+    if (numbers[i] === minNum) {
+      count++;
+    }
   }
-  return items;
+  return count;
 }
 
-console.log(shuffleItems([1, 2]));
+console.log(occurences(numbers));
