c1f6ff53ffd8db3bd18f1f50bc871ef8268cd106
Mario Milošević <mario.milosevic@rbt.rs>
2025-11-03 20:47:37 +0100

    palidrome alghoritm

--- monsterlessons/script.js
+++ monsterlessons/script.js
@@ -1,43 +1,15 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+// palidrome madam racecar level
 
-// Map Data in Promises
-// – Q1: You have 2 functions which return promises. Map data from
-// getUsers and getUserStatuses to get array of users with id, name,
-// isActive
-const users = [
-  { id: 1, name: "Jack" },
-  { id: 2, name: "John" },
-  { id: 3, name: "Mike" },
-];
-const userStatuses = [
-  { id: 1, isActive: true },
-  { id: 2, isActive: true },
-  { id: 3, isActive: false },
-];
-const getUsers = () => {
-  return new Promise((resolve) => {
-    resolve(users);
-  });
-};
-const getUserStatuses = () => {
-  return new Promise((resolve) => {
-    resolve(userStatuses);
-  });
-};
-
-async function fetchData() {
-  const [userStatuses, users] = await Promise.all([
-    getUserStatuses(),
-    getUsers(),
-  ]);
-
-  const result = userStatuses.map((status) => {
-    const user = users.find((user) => user.id === status.id);
-    user.isActive = status.isActive;
-    return user;
-  });
-
-  console.log(result);
+function palindrome(word) {
+  const sameLetters = [];
+  const characters = word.split("");
+  const reversedCharachters = [...characters].reverse();
+  for (let i = 0; i < characters.length; i++) {
+    if (characters[i] === reversedCharachters[i]) {
+      sameLetters.push(characters[i]);
+    }
+  }
+  return sameLetters;
 }
 
-fetchData();
+console.log(palindrome("mario"));
