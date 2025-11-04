405cda24bbca86bcff1c0a8b356e5b925f4879d4
Mario Milošević <mario.milosevic@rbt.rs>
2025-11-04 21:52:44 +0100

    palindrome alghorithm

--- monsterlessons/script.js
+++ monsterlessons/script.js
@@ -1,15 +1,9 @@
-// palidrome madam racecar level
-
 function palindrome(word) {
-  const sameLetters = [];
-  const characters = word.split("");
-  const reversedCharachters = [...characters].reverse();
-  for (let i = 0; i < characters.length; i++) {
-    if (characters[i] === reversedCharachters[i]) {
-      sameLetters.push(characters[i]);
-    }
-  }
-  return sameLetters;
+  const reversed = word.split("").reverse().join("");
+  return word === reversed;
 }
 
 console.log(palindrome("mario"));
+console.log(palindrome("level"));
+console.log(palindrome("racecar"));
+console.log(palindrome("madam"));
