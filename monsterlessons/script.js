bb9997e660497b0bf5db1ef45e26a4f2b3f6394c
Mario Milošević <mario.milosevic@rbt.rs>
2025-11-04 22:02:07 +0100

    find vowels in a word alghoritm

--- monsterlessons/script.js
+++ monsterlessons/script.js
@@ -1,9 +1,19 @@
-function palindrome(word) {
-  const reversed = word.split("").reverse().join("");
-  return word === reversed;
+const vowels = ["a", "e", "i", "o", "u"];
+
+function countVowels(word) {
+  const letters = word.split("");
+  console.log(letters);
+  let count = 0;
+  for (let i = 0; i < letters.length; i++) {
+    const letter = letters[i];
+    const isVowel = vowels.find((vowel) => vowel === letter);
+    if (isVowel) {
+      count++;
+      console.log(isVowel);
+    }
+  }
+  console.log(count);
+  return count;
 }
 
-console.log(palindrome("mario"));
-console.log(palindrome("level"));
-console.log(palindrome("racecar"));
-console.log(palindrome("madam"));
+countVowels("michelangelo");
