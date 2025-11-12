72217496c42e48dc04cbc6cf007c344e53ade749
Mario Milošević <mario.milosevic@rbt.rs>
2025-11-12 11:22:59 +0100

    Refactor more optimised solution

--- src/main.ts
+++ src/main.ts
@@ -26,16 +26,32 @@ setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
 const numbers = [1, 3, 7, 9, 2];
 const target = 11;
 
+// const findIndexesByTarget = (nums: number[], target: number) => {
+//   for (let p1 = 0; p1 < nums.length; p1++) {
+//     for (let p2 = 1; p2 < nums.length; p2++) {
+//       if (nums[p1] + nums[p2] === target) {
+//         return [p1, p2];
+//       }
+//     }
+//   }
+//   return null;
+// };
+
+// const firstResult = findIndexesByTarget(numbers, target);
+// console.log(firstResult);
+
 const findIndexesByTarget = (nums: number[], target: number) => {
+  const mapping: { [key: number]: number } = {};
   for (let p1 = 0; p1 < nums.length; p1++) {
-    for (let p2 = 1; p2 < nums.length; p2++) {
-      if (nums[p1] + nums[p2] === target) {
-        return [p1, p2];
-      }
+    const currentMappingIndex = mapping[nums[p1]];
+    if (currentMappingIndex >= 0) {
+      return [currentMappingIndex, p1];
+    } else {
+      mapping[target - nums[p1]] = p1;
     }
   }
   return null;
 };
 
-const result = findIndexesByTarget(numbers, target);
-console.log(result);
+const firstResult = findIndexesByTarget(numbers, target);
+console.log(firstResult);
