58efb59ed6f504bbefbf2b47e4d74a1b78e38ea9
Mario Milošević <mario.milosevic@rbt.rs>
2025-12-06 15:56:52 +0100

    exercise

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,1 @@
-// Singleton pattern
-
-class Config {
-  private static instance: Config;
-
-  private constructor() {}
-
-  public static getInstance() {
-    if (!Config.instance) {
-      Config.instance = new Config();
-    }
-    return Config.instance;
-  }
-}
+console.log("radi");
