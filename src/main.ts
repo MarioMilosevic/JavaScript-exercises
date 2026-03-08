6ee4f61cc9a09b6cbfea451d798c5d27cadfb139
Mario Milošević <mario.milosevic@rbt.rs>
2026-03-08 19:13:06 +0100

    finish first exercise

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,23 @@
-console.log("radi");
+// Mapping Users to Get Usernames
+// – Q1: Write code to get an array of names from given array of users
+const users = [
+  { id: 1, name: "Jack", isActive: true },
+  { id: 2, name: "John", isActive: true },
+  { id: 3, name: "Mike", isActive: false },
+];
+// Result
+// ['Jack','John','Mike']
 
-console.log("test branch");
-console.log("main branch");
+interface UserType {
+  id: number;
+  name: string;
+  isActive: boolean;
+}
+
+function convertUsers(users: UserType[]) {
+  const names = users.map((user) => user.name);
+  return names;
+}
+
+const names = convertUsers(users);
+console.log(names);
