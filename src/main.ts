827f4bfb3bcb865dd353724e54c08dd1721023e0
Mario Milošević <mario.milosevic@rbt.rs>
2026-03-08 19:29:01 +0100

    second exercise

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,18 @@
-// Mapping Users to Get Usernames
-// – Q1: Write code to get an array of names from given array of users
-const users = [
-  { id: 1, name: "Jack", isActive: true },
-  { id: 2, name: "John", isActive: true },
-  { id: 3, name: "Mike", isActive: false },
-];
-// Result
-// ['Jack','John','Mike']
-
-interface UserType {
-  id: number;
-  name: string;
-  isActive: boolean;
+function privateCounter() {
+  let count = 0;
+  const getValue = () => {
+    return count;
+  };
+  const increment = () => {
+    count = count + 1;
+  };
+  return {
+    getValue,
+    increment,
+  };
 }
 
-function convertUsers(users: UserType[]) {
-  const names = users.map((user) => user.name);
-  return names;
-}
-
-const names = convertUsers(users);
-console.log(names);
+const counter = privateCounter();
+console.log(counter.getValue());
+counter.increment();
+console.log(counter.getValue());
