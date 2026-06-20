b59b1f90725d372dcd7e92667552784eca260cd5
Mario Milošević <mario.milosevic@rbt.rs>
2026-06-20 16:33:21 +0200

    some

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-function startCounter() {
-  for (let i = 1; i <= 3; i++) {
-    setTimeout(function () {
-      console.log("Count:", i);
-    }, i * 1000);
-  }
-}
+// Q1: Write a function which accepts a list of users and a name to
+// check if such user exists in the array.
+const users = [
+  { id: 1, name: "Jack", isActive: true },
+  { id: 2, name: "John", isActive: true },
+  { id: 3, name: "Mike", isActive: false },
+];
 
-startCounter();
-// expected: Count: 1, Count: 2, Count: 3 (one per second)
+function isNameExists(
+  name: string,
+  names: { id: number; name: string; isActive: boolean }[],
+) {
+  return names.some((n) => n.name === name);
+}
+console.log(isNameExists("Jack", users)); // true
