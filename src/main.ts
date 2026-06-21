028b1acb90f497e3fd4e1f671cc1d2cac91571fc
Mario Milošević <mario.milosevic@rbt.rs>
2026-06-21 21:04:51 +0200

    event delegation

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,9 @@
-// – Q1: Create a throttle function
+//  Event Delegation
+// – Q1: Implement a click on todo item which has a high performance.
 
-function throttle(cb: () => void, time: number) {
-  let canCall = true;
-  return function (...args) {
-    if (canCall) {
-      cb(...args);
-      canCall = false;
-      setTimeout(() => {
-        canCall = true;
-      }, time);
-    }
-  };
-}
+const list = document.querySelector(".todo-app");
 
-const saveInput = (name) => console.log("saveInput", name);
-const processChange = throttle(saveInput, 2000);
-
-processChange("foo");
-setTimeout(() => {
-  processChange("foo");
-}, 1000);
-setTimeout(() => {
-  processChange("foo");
-}, 1200);
-setTimeout(() => {
-  processChange("foo");
-}, 2400);
-processChange("foo");
-processChange("foo");
+list?.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(e.target.innerText);
+});
