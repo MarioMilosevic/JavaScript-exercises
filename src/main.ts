dfed807e87196eddf4b6b71926d586278ec683d0
Mario Milošević <mario.milosevic@rbt.rs>
2026-06-29 15:32:29 +0200

    vjezba

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,57 @@
-// Q1: Design a function which returns a fibonacci sequence value
+//  Q1: Map data to frontend format. The main element is location key
+// and we need to map all data to it.
 
-// 0 + 1
-// a + b = result
-// result + result = newResult
-//  newResult = newResult + newResult
+function solution(location, config) {
+  const solution = [
+    { config_key: 100, location_key: 32, autoassign: 1 },
+    {
+      config_key: 100,
+      location_key: 22,
+      autoassign: 1,
+    },
+  ];
+  const all = [];
 
-// 1,2,3,5,8,13
-
-function fibonnaci() {
-  const numbers = [];
-  let firstNumber = 0;
-  let secondNumber = 1;
-  for (let i = 0; i < 10; i++) {
-    const nextNumber = firstNumber + secondNumber;
-    firstNumber = secondNumber;
-    secondNumber = nextNumber;
-    numbers.push(nextNumber);
+  for (let i = 0; i < location.length; i++) {
+    const { location_key, autoassign } = location[i];
+    for (let j = 0; j < location_key.length; j++) {
+      const obj = {
+        location_key: location_key[j],
+        config_key: config[i].dataValues.config_key,
+        autoassign: autoassign,
+      };
+      all.push(obj);
+    }
   }
-  return numbers;
+  return all;
 }
 
-console.log(fibonnaci());
+const loc = [
+  {
+    /*
+      za svaki objekat, da odradim for petlju za location_key
+      gdje cu napraviti objekat 
+      da config_key, bude element[i] i dodijeli tu vrijednost location elementu[i]
+      */
+    location_key: [32, 22, 11],
+    autoassign: 1,
+  },
+  {
+    location_key: [41, 42],
+    autoassign: 1,
+  },
+];
+const bulkConfig = [
+  {
+    dataValues: {
+      config_key: 100,
+    },
+  },
+  {
+    dataValues: {
+      config_key: 200,
+    },
+  },
+];
+
+console.log(solution(loc, bulkConfig));
