6938e80a401069b1810be81c29f2abbb7af41df5
Mario Milošević <mario.milosevic@rbt.rs>
2026-07-05 16:54:43 +0200

    edde

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log("mario");
+class Config {
+  constructor() {
+    this.name = "config";
+  }
+
+  createConfig() {
+    if (!this.name) return new Config();
+    else return this.name;
+  }
+
+  getConfigName() {
+    return this.name;
+  }
+}
