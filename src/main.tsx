fc2af59290e61a704ce39589d235ede23bb1a3cd
Mario Milošević <mario.milosevic@rbt.rs>
2026-07-06 14:27:42 +0200

    singleton

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+class Config {
+  private static instance: Config;
+
+  private constructor() {}
+
+  public static getInstance(): Config {
+    if (!Config.instance) {
+      Config.instance = new Config();
+    }
+    return Config.instance;
+  }
+}
